ea484a0ffa061e1980588094f726c2566c818fed
karth <karth@Karthik-HP>
2017-07-04 14:42:55 +0530

    Added comments to the program

--- src/JavaSession1Assignment4/TypeCastingClass.java
+++ src/JavaSession1Assignment4/TypeCastingClass.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class TypeCastingClass {
 
 	public static void main(String[] args) {
+		//Defining six variables for six data types
 		byte b;
 		short s;
 		int i;
@@ -12,19 +13,19 @@ public class TypeCastingClass {
 		float f;
 		double d;
 		@SuppressWarnings("resource")
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in); //Scanner class is used to accept input from user
 		System.out.println("Enter the value for byte variable");
 		b = input.nextByte();
 		System.out.println("Enter the value for short variable");
 		s = input.nextShort();
 		
-		i = b + s;
+		i = b + s; //Type-casting is performed to integer type from byte and short type
 		System.out.println("Sum of byte and short in int: "+i);
-		l = s + i;
+		l = s + i; //Type-casting is performed to long type from short and integer type
 		System.out.println("Sum of short and int in long: "+l);
-		f = i + l;
+		f = i + l; //Type-casting is performed to float type from integer and long type
 		System.out.println("Sum of int and long in float: "+f);
-		d = l + f;
+		d = l + f; //Type-casting is performed to double type from long and float type
 		System.out.println("Sum of long and float in double: "+d);
 	}
 
